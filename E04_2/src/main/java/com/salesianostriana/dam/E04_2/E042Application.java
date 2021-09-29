04b12157f136ddb4ea5d9b53cb4bfd76292bd5a1
vicente1991 <74652123+vicente1991@users.noreply.github.com>
2021-09-29 16:21:38 +0200

    Add files via upload
    descompresion de E04_2

---
+++ E04_2/src/main/java/com/salesianostriana/dam/E04_2/E042Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E04_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E042Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E042Application.class, args);
+	}
+
+}
---
+++ E04_2/src/test/java/com/salesianostriana/dam/E04_2/E042ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E04_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E042ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
