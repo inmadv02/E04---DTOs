5bc6bac4135cdbf3209500222f240da5780d484e
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-09-29 16:46:32 +0200

    Creación ProductoDTO

--- E04/src/main/java/com/salesianostriana/dam/E04/controller/AlumnoController.java
+++ E04/src/main/java/com/salesianostriana/dam/E04/controller/AlumnoController.java
@@ -1,9 +1,15 @@
 package com.salesianostriana.dam.E04.controller;
 
+import com.salesianostriana.dam.E04.dto.GetAlumnoDto;
+import com.salesianostriana.dam.E04.modelo.Alumno;
 import com.salesianostriana.dam.E04.repositorio.AlumnoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlumnoController {
 
     private final AlumnoRepository repository;
+
+    public ResponseEntity<List<Alumno>> findAll() {
+
+        return ResponseEntity
+                .ok()
+                .body(repository.findAll());
+
+    }
 
 }
---
+++ E04_2/src/main/java/com/salesianostriana/dam/E04_2/dto/GetProductoDTO.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.dam.E04_2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetProductoDTO {
+
+    private String nombre;
+    private double pvp;
+    private String imagen;
+    //private Categoria categoria;
+
+}
