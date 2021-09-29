f69f6434896c1071cf3606e70cc10264088cac47
María Inmaculada Domínguez Vargas <dominguez.vamar21@triana.salesianos.edu>
2021-09-29 12:47:50 +0200

    Creación del proyecto

---
+++ E04/src/main/java/com/salesianostriana/dam/E04/E04Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.E04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E04Application.class, args);
+	}
+
+}
